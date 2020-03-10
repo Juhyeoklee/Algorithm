abac49ad6a3cbab62a302fc334c9b960b9070f4a
Juhyeoklee <gur6531@naver.com>
2020-03-11 01:59:54 +0900

    [1000] complete

---
+++ BaekJoon/1000.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("%d\n", a + b);
+
+    return 0;
+}
