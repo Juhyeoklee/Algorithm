b5bb8542bfb1f36c5b750f21681a35c35db553c0
Juhyeoklee <gur6531@naver.com>
2020-03-30 00:44:58 +0900

    [2743] 단어길이재기

---
+++ BaekJoon/자료구조1/2743_단어길이재기.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s.size() << endl;
+
+    // char *s1;
+    // scanf("%s", s1);
+    // int len = 0;
+    // for (int i = 0; s1[i]; i++)
+    // {
+    //     len++;
+    // }
+    // printf("%d\n", len);
+
+    // char *s2;
+    // scanf("%s", s2);
+    // int len = strlen(s2);
+
+    // printf("%d\n", len);
+    return 0;
+}
